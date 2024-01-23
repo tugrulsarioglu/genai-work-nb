15ff0177f0e7411c466a1033e083ef0960e9f20f
okanyenigun <okanyenigun@gmail.com>
2024-01-24 02:11:37 +0300

    first commit

---
+++ sentiment_code.py
@@ -0,0 +1,30 @@
+import multiprocessing
+import pandas as pd
+from transformers import pipeline
+
+MODEL = "nlptown/bert-base-multilingual-uncased-sentiment"
+df = pd.read_excel("ING_appstore_reviews.xlsx")
+
+def analyze_sentiment(args):
+    index, text = args
+    try:
+        result = sentiment_pipe(text)[0]
+        print(f"Row {index}: Label - {result['label']}, Score - {result['score']}")
+        return result["label"], result["score"]
+    except Exception as e:
+        print(f"Error processing text in row {index}: {e}")
+        return None, None
+
+sentiment_pipe = pipeline("text-classification", model=MODEL)
+
+cpu_count = multiprocessing.cpu_count()
+num_processes = max(1, cpu_count - 1)
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        index_text_pairs = zip(df.index, df["deep_translator"])
+        results = pool.map(analyze_sentiment, index_text_pairs)
+
+    df['label'], df['score'] = zip(*results)
+
+df.to_excel("ing.xlsx",index=False)
